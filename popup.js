74dad79dc15da02079c6707dd1adbbde0482e49d
JamshedVesuna <jamshed.vesuna@gmail.com>
2023-12-24 21:35:05 -0800

    Cards

--- popup.js
+++ popup.js
@@ -14,9 +14,14 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   populateAccountDropdown();
+
   const createAccountButton = document.getElementById('createAccountButton');
   createAccountButton.addEventListener('click', createNewAccountNumber);
+
+  const createCardButton = document.getElementById('createCardButton');
+  createCardButton.addEventListener('click', createNewCard);
 });
+
 
 function hideApiKeyPrompt() {
   document.getElementById('apiKeyInput').style.display = 'none';
@@ -53,22 +58,14 @@ function populateAccountDropdown() {
   })
 }
 
+
 function createNewAccountNumber() {
   const apiUrl = 'https://sandbox.increase.com/account_numbers';
-
   const accountSelect = document.getElementById('accountSelect');
   const selectedAccountId = accountSelect.options[accountSelect.selectedIndex].value;
-
   const accountNumberName = document.getElementById('accountNumberNameInput').value.trim();
   const today = new Date().toISOString().slice(0,10).replace(/-/g,"");
   const accountNumberNameToUse = accountNumberName || `AccountNumber ${today}`;
-
-
-  const requestBody = {
-    'account_id': selectedAccountId,
-    'name': accountNumberNameToUse
-  };
-
   chrome.storage.local.get('apiKey', function(data) {
     const apiKey = data.apiKey;
     fetch(apiUrl, {
@@ -84,15 +81,48 @@ function createNewAccountNumber() {
     })
       .then(response => response.json())
       .then(data => {
-        const resultDiv = document.getElementById('result');
+        const resultDiv = document.getElementById('account-number-result');
         resultDiv.innerHTML = 'Account number: ' + data.account_number + '<br>Routing number: ' + data.routing_number; // Replace with the actual response property name
-
       })
       .catch(error => {
         console.error('Error:', error);
         alert('Error creating account number. Please try again.');
       });
   })
-
 }
 
+
+function createNewCard() {
+  const apiUrl = 'https://sandbox.increase.com/cards';
+  const accountSelect = document.getElementById('accountSelect');
+  const selectedAccountId = accountSelect.options[accountSelect.selectedIndex].value;
+  const cardDescription = document.getElementById('cardDescriptionInput').value.trim();
+  const today = new Date().toISOString().slice(0,10).replace(/-/g,"");
+  const cardDescriptionToUse = cardDescription || `Card ${today}`;
+
+  chrome.storage.local.get('apiKey', function(data) {
+    const apiKey = data.apiKey;
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'account_id': selectedAccountId,
+        'description': cardDescriptionToUse
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Card created:', data);
+        const resultDiv = document.getElementById('card-result');
+        resultDiv.innerHTML = 'Card number: ' + data.last4;
+        // Handle the response data here
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        alert('Error creating card. Please try again.');
+      });
+  });
+}
